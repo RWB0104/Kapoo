f570d5b1da1f42668634915ea479df6fc733b087
RWB0104 <psj1789@naver.com>
2021-09-14 02:22:14 +0900

    빌드 1f3a3a07344 버전  - Screener 텍스트 크기 변경  - ContentItem 텍스트 크기 축소  - ContentCategory 이미지 일관성 준수하도록 개선  - ContentCategory 데스크탑 기준 줄 당 4칸 -> 8칸 변경  - ContentCategory 모바일 기준 줄 당 2칸 -> 4칸 변경  - 타이틀 변경  - ContentItem 이미지 높이 축소  - 다음/이전 게시글 라우팅 시 Content Screener 이미지 미반영 현상 수정 (useEffect 로직 변경)

--- components/global/Screener.tsx
+++ components/global/Screener.tsx
@@ -39,7 +39,7 @@ export default function Screener({ title, lower, image, special }: Props): React
 		{
 			ref.current.style.backgroundImage = `url(${image})`;
 		}
-	}, []);
+	}, [ image ]);
 
 	return (
 		<Box className={styles.root}>
