27049c48ddd3d139980e7333455f11d2e326f03d
박성진 <psj2716@gmail.com>
2022-07-15 10:37:12 +0900

    :hammer: [UPDATE] 45f4734c1e0 커밋
    - GA Refresh 토큰 변경

--- commons/ga.ts
+++ commons/ga.ts
@@ -60,7 +60,7 @@ export async function authorize(): Promise<GoogleAuth | undefined>
 			client_id: '22130300203-s47tft38ah28e6o2jsv5144vqn1cl32p.apps.googleusercontent.com',
 			client_secret: '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',
 			grant_type: 'refresh_token',
-			refresh_token: '1//04luNmTJNLtHKCgYIARAAGAQSNwF-L9IrN1i1EXdiK3_w21-ILz2PjgOtfsw0GBdavxD52moN0hxFrSEFFgcz_15wUrXcGYcs1pk'
+			refresh_token: '1//04iZ6ahFjojgzCgYIARAAGAQSNwF-L9IrOhYq9Wz4MycJH_H2CyuxxhCntZeHhmfsLwfIXgJzV-11rEvDZLmEFNl8k_UnR3LpCOs'
 		}),
 		method: 'POST'
 	});
