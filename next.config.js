4fa15ffc6b5f360c1c2e66735bfd461314fbed57
RWB0104 <psj2716@gmail.com>
2023-02-04 12:39:48 +0900

    🔧 [UPDATE] PWA 관련 설정 개선

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 const withSass = require('@zeit/next-sass');
 const withPWA = require('next-pwa');
 
-module.exports = withSass({
-	cssModules: true
-});
-
-module.exports = {
-	images: {
-		loader: 'imgix'
-	},
-	productionBrowserSourceMaps: false,
-	reactStrictMode: false
-};
+module.exports = withSass({ cssModules: true });
 
 module.exports = withPWA({
-	pwa: {
-		dest: 'public',
-		register: true,
-		skipWaiting: true
-	}
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
 });
