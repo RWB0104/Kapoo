68e90bebbd57575a0693448e82714cc9646f7731
RWB0104 <psj2716@gmail.com>
2023-02-07 02:10:44 +0900

    🔧 [UPDATE] Nextjs 설정 갱신

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
+const withPWA = require('next-pwa')({
 	dest: 'public',
 	disable: process.env.NODE_ENV === 'development'
+});
+
+module.exports = withPWA({
+	reactStrictMode: true,
+	swcMinify: true
 });
