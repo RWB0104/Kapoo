ecc417bfb2f6018f5079eb323d586a946a60a6f4
RWB0104 <psj2716@mensakorea.org>
2023-08-20 02:45:25 +0900

    💡 [UPDATE] 코드 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+/**
+ * 홈 페이지 컴포넌트
+ *
+ * @author RWB
+ * @since 2023.08.19 토 19:58:12
+ */
+
 import PageTemplate from '@kapoo/template/global/PageTemplate';
 import HomeScreenerTemplate from '@kapoo/template/home/HomeScreenerTemplate';
 
 import Typography from '@mui/material/Typography';
 import { ReactNode } from 'react';
 
+/**
+ * 홈 페이지 컴포넌트 JSX 반환 메서드
+ *
+ * @returns {ReactNode} ReactNode
+ */
 export default function HomePage(): ReactNode
 {
 	return (
