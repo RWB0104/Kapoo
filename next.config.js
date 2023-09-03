89e1a011835e35cfbfd943f9fc3e37b25de7e92a
RWB0104 <psj2716@mensakorea.org>
2023-09-04 02:29:25 +0900

    🔧 [UPDATE] PWA 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = withPWA({ output: 'export' });
 
 module.exports = nextConfig;
