1d1dc864806414f6ce0baf0c3b20d2e3f13d23f0
RWB0104 <psj2716@mensakorea.org>
2023-09-04 03:05:08 +0900

    🔨 [UPDATE] manifest 아이콘 설정 변경

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,9 +17,45 @@ export default function manifest(): MetadataRoute.Manifest
 		display: 'standalone',
 		icons: [
 			{
-				sizes: 'any',
-				src: '/favicon.ico',
-				type: 'image/x-icon'
+				sizes: '72x72',
+				src: '/icons/icon-72x72.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '96x96',
+				src: '/icons/icon-96x96.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '128x128',
+				src: '/icons/icon-128x128.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '144x144',
+				src: '/icons/icon-144x144.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '152x152',
+				src: '/icons/icon-152x152.png',
+				type: 'image/png'
+			},
+			{
+				purpose: 'any',
+				sizes: '192x192',
+				src: '/icons/icon-192x192.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '384x384',
+				src: '/icons/icon-384x384.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '512x512',
+				src: '/icons/icon-512x512.png',
+				type: 'image/png'
 			}
 		],
 		name: APP_INFO.title,
