9f351c12a64f38310d3f6c7d5b77e750d751b64d
RWB0104 <psj2716@mensakorea.org>
2023-09-10 01:08:09 +0900

    ✨ [FEATURE] 뷰 광고 템플릿 컴포넌트 추가

---
+++ src/component/template/view/ViewAdsTemplate/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 뷰 템플릿 template 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2023.09.10 Sun 00:55:02
+ */
+
+import ViewAdsTemplate from './ViewAdsTemplate';
+
+export default ViewAdsTemplate;
