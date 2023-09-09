6bd86df727ce80227848e1d6e13667c16e261b5f
RWB0104 <psj2716@mensakorea.org>
2023-09-10 00:33:06 +0900

    ✨ [FEATURE] 애드센스 컴포넌트 추가

---
+++ src/component/atom/Ads/Ads.tsx
@@ -0,0 +1,32 @@
+/**
+ * 애드센스 atom 컴포넌트
+ *
+ * @author RWB
+ * @since 2023.09.10 Sun 00:32:14
+ */
+
+'use client';
+
+import { Adsense } from '@ctrl/react-adsense';
+import { ReactNode } from 'react';
+
+/**
+ * 애드센스 atom 컴포넌트 JSX 반환 메서드
+ *
+ * @returns {ReactNode} ReactNode
+ */
+export default function Ads(): ReactNode
+{
+	return (
+		<Adsense
+			adTest='on'
+			client='ca-pub-5522045122225064'
+			data-component='Ads'
+			format='fluid'
+			layout='in-article'
+			responsive='true'
+			slot='8348565597'
+			style={{ display: 'block' }}
+		/>
+	);
+}
---
+++ src/component/atom/Ads/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 애드센스 atom 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2023.09.10 Sun 00:32:40
+ */
+
+import Ads from './Ads';
+
+export default Ads;
