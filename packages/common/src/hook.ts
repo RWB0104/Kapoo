ebc358dcdcfd242ffe597596b56a59f81dbd0889
RWB0104 <psj2716@mensakorea.org>
2024-04-01 18:25:12 +0900

    🔨 [FEATURE] 신규 스크립트 추가 - 훅 - 타입

---
+++ packages/common/src/hook.ts
@@ -0,0 +1,42 @@
+/**
+ * 훅 모듈
+ *
+ * @author RWB
+ * @since 2024.04.01 Mon 09:46:48
+ */
+
+import { useEffect } from 'react';
+
+export type UseIntersectionObserverCallback = (isShow: boolean) => void;
+
+/**
+ * IntersectionObserver 적용 훅 메서드
+ *
+ * @param {Element | null} ref: Element
+ * @param {UseIntersectionObserverCallback} callback: 콜백 메서드
+ * @param {IntersectionObserverInit} options: 옵션
+ */
+export function useIntersectionObserver(ref: Element | null, callback: UseIntersectionObserverCallback, options?: IntersectionObserverInit): void
+{
+	useEffect(() =>
+	{
+		const io = new IntersectionObserver((entries) =>
+		{
+			entries.forEach((entry) =>
+			{
+				callback?.(entry.isIntersecting);
+			});
+		}, options);
+
+		// DOM이 유효할 경우
+		if (ref)
+		{
+			io.observe(ref);
+		}
+
+		return () =>
+		{
+			io.disconnect();
+		};
+	}, [ ref ]);
+}
---
+++ packages/common/src/type.ts
@@ -0,0 +1,8 @@
+/**
+ * 타입 모듈
+ *
+ * @author RWB
+ * @since 2024.04.01 Mon 09:20:31
+ */
+
+export type Func<T> = Exclude<T, undefined>;
