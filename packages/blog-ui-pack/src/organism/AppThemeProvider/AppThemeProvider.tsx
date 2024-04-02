a1f34fefe21a3ec965b88a290ffa8dacf1b34545
RWB0104 <psj2716@mensakorea.org>
2024-04-03 00:03:46 +0900

    ✨ [FEATURE] 신규 컴포넌트 추가 - 블로그 앱 테마 프로바이더

---
+++ packages/blog-ui-pack/src/organism/AppThemeProvider/AppThemeProvider.tsx
@@ -0,0 +1,37 @@
+/**
+ * 앱 테마 프로바이더 organism 컴포넌트
+ *
+ * @author RWB
+ * @since 2024.04.02 Tue 23:49:08
+ */
+
+'use client';
+
+import { themeStore } from '@kapoo/state';
+import { Button, PaletteMode, ThemeProvider, createTheme } from '@mui/material';
+import { PropsWithChildren, ReactNode, useCallback } from 'react';
+
+/**
+ * 앱 테마 프로바이더 organism 컴포넌트 반환 메서드
+ *
+ * @param {PropsWithChildren} param0: PropsWithChildren
+ *
+ * @returns {ReactNode} ReactNode
+ */
+export default function AppThemeProvider({ children }: PropsWithChildren): ReactNode
+{
+	const { theme, toggleTheme } = themeStore();
+
+	const getTheme = useCallback((theme: PaletteMode) => createTheme({
+		palette: { mode: theme },
+		typography: { fontFamily: 'Pretendard, sans-serif' }
+	}), []);
+
+	return (
+		<ThemeProvider theme={getTheme(theme)}>
+			{children}
+			{theme}
+			<Button onClick={toggleTheme}>테스트</Button>
+		</ThemeProvider>
+	);
+}
---
+++ packages/blog-ui-pack/src/organism/AppThemeProvider/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 앱 테마 프로바이더 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.04.02 Tue 23:50:23
+ */
+
+import AppThemeProvider from './AppThemeProvider';
+
+export default AppThemeProvider;
+
+export * from './AppThemeProvider';
