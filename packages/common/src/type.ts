72b979a717db120288d419bb88963f9b57b3e04d
RWB0104 <psj2716@mensakorea.org>
2024-04-02 18:15:11 +0900

    🏷️ [UPDATE] 타입 갱신

--- packages/common/src/type.ts
+++ packages/common/src/type.ts
@@ -5,4 +5,8 @@
  * @since 2024.04.01 Mon 09:20:31
  */
 
+export interface NextPageProps<T = Record<string, string>, K = Record<string, string>> {
+	params: T;
+	searchParams?: K;
+}
 export type Func<T> = Exclude<T, undefined>;
