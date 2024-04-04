c26ef74f6fae30f9c92710205f667396356bbb8e
RWB0104 <psj2716@mensakorea.org>
2024-04-04 18:21:52 +0900

    🏷️ [FEATURE] blog 전역 타입 신규 추가

---
+++ apps/blog/global.d.ts
@@ -0,0 +1,54 @@
+type MarkdownType = 'posts' | 'projects';
+
+interface MarkdownHeaderProps
+{
+	/**
+	 * 제목
+	 */
+	title: string;
+
+	/**
+	 * 요약
+	 */
+	excerpt: string;
+
+	/**
+	 * 썸네일
+	 */
+	coverImage: string;
+
+	/**
+	 * 날짜
+	 */
+	date: number;
+
+	/**
+	 * 타입
+	 */
+	type: MarkdownType;
+
+	/**
+	 * 카테고리
+	 */
+	category: string;
+
+	/**
+	 * 태그
+	 */
+	tag: string[];
+
+	/**
+	 * 그룹
+	 */
+	group?: string;
+
+	/**
+	 * 댓글 여부
+	 */
+	comment: boolean;
+
+	/**
+	 * 발행 여부
+	 */
+	publish: boolean;
+}
