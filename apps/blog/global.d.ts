a25d78b977d6ccf1f9affdc3e72670965a269a7f
RWB0104 <psj2716@mensakorea.org>
2024-04-09 09:52:56 +0900

    🏷️ [UPDATE] 블로그 앱 ProcessEnv 타입 확장

--- apps/blog/global.d.ts
+++ apps/blog/global.d.ts
@@ -52,3 +52,30 @@ interface MarkdownHeaderProps
 	 */
 	publish: boolean;
 }
+
+declare module 'process'
+{
+	global
+	{
+		namespace NodeJS
+		{
+			interface ProcessEnv
+			{
+				/**
+				 * 타이틀
+				 */
+				NEXT_PUBLIC_TITLE: string;
+
+				/**
+				 * 설명
+				 */
+				NEXT_PUBLIC_DESCRIPTION: string;
+
+				/**
+				 * URL
+				 */
+				NEXT_PUBLIC_BASE_URL: string;
+			}
+		}
+	}
+}
