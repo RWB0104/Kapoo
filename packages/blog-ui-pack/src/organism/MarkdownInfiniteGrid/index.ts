85ba5b630fb0a87ef72aea82877eb5fdc3b93df9
RWB0104 <psj2716@mensakorea.org>
2024-04-24 02:21:30 +0900

    ✨ [FEATURE] 신규 컴포넌트 추가 - 마크다운 인피니트 그리드 - 타이틀 템플릿

---
+++ packages/blog-ui-pack/src/organism/MarkdownInfiniteGrid/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 마크다운 인피니트 그리드 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.04.24 Wed 00:16:07
+ */
+
+import MarkdownInfiniteGrid from './MarkdownInfiniteGrid';
+
+export default MarkdownInfiniteGrid;
+
+export * from './MarkdownInfiniteGrid';
---
+++ packages/blog-ui-pack/src/template/TitleTemplate/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 타이틀 template 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.04.24 Wed 00:05:07
+ */
+
+import TitleTemplate from './TitleTemplate';
+
+export default TitleTemplate;
+
+export * from './TitleTemplate';
