f4267d90bb342d0f159e303cc276abfed299cb00
RWB0104 <psj2716@mensakorea.org>
2024-05-02 18:20:23 +0900

    🔧 [UPDATE] next.config.js 개선

--- apps/root/next.config.js
+++ apps/root/next.config.js
@@ -1,18 +1,24 @@
-//@ts-check
-
+// @ts-check
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
+})
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
+ */
+// @ts-expect-error nx with PWA
+const nextConfig = withPWA({
+  output: 'export',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
+});
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
