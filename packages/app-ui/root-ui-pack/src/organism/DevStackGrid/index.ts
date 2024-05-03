14b95603d8e0c2a899d033e93bf6d5588060a838
RWB0104 <psj2716@mensakorea.org>
2024-05-03 17:31:46 +0900

    ✨ [FEATURE] 개발 스택 그리드 컴포넌트 신규 추가

---
+++ packages/app-ui/root-ui-pack/src/organism/DevStackGrid/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 개발 스택 그리드 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.05.03 Fri 17:25:17
+ */
+
+import DevStackGrid from './DevStackGrid';
+
+export default DevStackGrid;
+
+export * from './DevStackGrid';
