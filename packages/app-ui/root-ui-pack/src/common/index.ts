c575533a09611827b59b329d45c4c564a4d5e4f1
RWB0104 <psj2716@mensakorea.org>
2024-05-06 14:55:40 +0900

    🔨 [FEATURE] 루트 UI 공통 모듈 신규 추가

---
+++ packages/app-ui/root-ui-pack/src/common/index.ts
@@ -0,0 +1,72 @@
+/**
+ * 공통 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.05.05 Sun 03:26:50
+ */
+
+export interface MarkdownHeaderLink
+{
+	/**
+	 * 타입
+	 */
+	type: 'github' | 'web';
+
+	/**
+	 * 이름
+	 */
+	name: string;
+
+	/**
+	 * URL
+	 */
+	url: string;
+}
+
+export interface MarkdownHeaderProps
+{
+	/**
+	 * 제목
+	 */
+	title: string;
+
+	/**
+	 * 작가
+	 */
+	author: string;
+
+	/**
+	 * 부제목
+	 */
+	subtitle: string[];
+
+	/**
+	 * 아이콘
+	 */
+	icon: string;
+
+	/**
+	 * 언어
+	 */
+	languages: string[];
+
+	/**
+	 * 이미지
+	 */
+	images: string[];
+
+	/**
+	 * 링크
+	 */
+	links?: MarkdownHeaderLink[];
+
+	/**
+	 * 시작일자
+	 */
+	created: number;
+
+	/**
+	 * 완료일자
+	 */
+	completed?: number;
+}
