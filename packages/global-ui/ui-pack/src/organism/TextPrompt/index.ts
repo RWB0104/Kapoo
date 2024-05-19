07a52ce010046f3eefadc1023f21abb7a6fa8eed
RWB0104 <psj2716@mensakorea.org>
2024-05-20 02:53:11 +0900

    ✨ [FEATURE] 텍스트 프롬프트 컴포넌트 신규 추가

---
+++ packages/global-ui/ui-pack/src/organism/TextPrompt/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 텍스트 프롬프트 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.05.20 Mon 02:52:51
+ */
+
+import TextPropmt from './TextPrompt';
+
+export default TextPropmt;
+
+export * from './TextPrompt';
