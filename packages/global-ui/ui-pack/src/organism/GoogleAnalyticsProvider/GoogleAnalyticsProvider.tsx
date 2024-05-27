8329ac1cb6b92986e0b0038c08b0527742a7f52a
RWB0104 <psj2716@mensakorea.org>
2024-05-27 18:28:36 +0900

    ✨ [FEATURE] 신규 컴포넌트 추가 - 네임택 - GA 프로바이더

---
+++ packages/global-ui/ui-pack/src/organism/GoogleAnalyticsProvider/GoogleAnalyticsProvider.tsx
@@ -0,0 +1,31 @@
+/**
+ * GA 프로바이더 organism 컴포넌트
+ *
+ * @author RWB
+ * @since 2024.05.27 Mon 18:18:04
+ */
+
+import { PropsWithChildren } from 'react';
+import reactGA from 'react-ga4';
+
+export interface GoogleAnalyticsProviderProps extends PropsWithChildren
+{
+	/**
+	 * GA 키
+	 */
+	gaKey: string;
+}
+
+/**
+ * GA 프로바이더 organism 컴포넌트 반환 메서드
+ *
+ * @param {GoogleAnalyticsProviderProps} param0: GoogleAnalyticsProviderProps
+ *
+ * @returns {JSX.Element} JSX
+ */
+export default function GoogleAnalyticsProvider({ gaKey, children }: GoogleAnalyticsProviderProps): JSX.Element
+{
+	reactGA.initialize(gaKey);
+
+	return <>{children}</>;
+}
---
+++ packages/global-ui/ui-pack/src/organism/GoogleAnalyticsProvider/index.ts
@@ -0,0 +1,12 @@
+/**
+ * GA 프로바이더 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.05.27 Mon 18:18:52
+ */
+
+import GoogleAnalyticsProvider from './GoogleAnalyticsProvider';
+
+export default GoogleAnalyticsProvider;
+
+export * from './GoogleAnalyticsProvider';
