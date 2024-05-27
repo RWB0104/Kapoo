6a11e1b7b3c96e7be7c81450f1f9aeccbdaf5b3f
RWB0104 <psj2716@mensakorea.org>
2024-05-27 23:00:21 +0900

    ✨ [FEATURE] 연구소 네임택 컴포넌트 신규 추가

---
+++ packages/app-ui/blog-ui-pack/src/organism/LabNameTag/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 연구소 네임택 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.05.27 Mon 22:45:25
+ */
+
+import LabNameTag from './LabNameTag';
+
+export default LabNameTag;
+
+export * from './LabNameTag';
