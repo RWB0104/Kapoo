9037cbaf5c4fb9583a1c0619e75189627d4231b7
RWB0104 <psj2716@mensakorea.org>
2024-06-03 01:45:15 +0900

    🚧 [PROGRESS] 캐러셀 작업

--- packages/global-ui/ui-pack/src/organism/Carousel/Carousel.tsx
+++ packages/global-ui/ui-pack/src/organism/Carousel/Carousel.tsx
@@ -9,12 +9,10 @@
 
 import { modulo } from '@kapoo/common';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import { useEffect, useState } from 'react';
-
-import CarouselItem from './sub/CarouselItem';
+import { CSSProperties, useState } from 'react';
 
 export type CarouselRenderHandler = (index: number) => JSX.Element;
+export type CarouselMoveHandler = (flag: 'left' | 'right') => void;
 
 export interface CarouselProps
 {
@@ -24,14 +22,19 @@ export interface CarouselProps
 	defaultIndex?: number;
 
 	/**
-	 * 인덱스
-	 */
-	index?: number;
-
-	/**
 	 * 전체 갯수
 	 */
 	total: number;
+
+	/**
+	 * 너비
+	 */
+	width?: CSSProperties['width'];
+
+	/**
+	 * 높이
+	 */
+	height?: CSSProperties['height'];
 
 	/**
 	 * 렌더링 핸들러
@@ -39,50 +42,99 @@ export interface CarouselProps
 	children: CarouselRenderHandler;
 }
 
-export default function Carousel({ defaultIndex = 0, index, total, children }: CarouselProps): JSX.Element
+export default function Carousel({ defaultIndex = 0, total, width = '100%', height = '100%', children }: CarouselProps): JSX.Element
 {
-	const [ trackRefState, setTrackRefState ] = useState<HTMLDivElement | null>(null);
-	const [ indexState, setIndexState ] = useState(defaultIndex);
+	const [ currCarousel, setCurrCarousel ] = useState(defaultIndex);
+	const [ carouselTransition, setCarouselTransition ] = useState('500ms ease-in-out');
+	const [ delayState, setDelayState ] = useState(false);
 
-	useEffect(() =>
+	const slideNextSoulsCarousel = (): void =>
 	{
-		const tag = document.querySelector<HTMLDivElement>('[data-component="test"]');
-
-		if (trackRefState && tag)
+		if (!delayState)
 		{
-			trackRefState.style.transform = `translateX(${-tag.scrollWidth}px)`;
-		}
-	}, [ trackRefState ]);
+			setDelayState(true);
 
-	useEffect(() =>
+			const soulSliderLength = total;
+
+			const newCurr = currCarousel + 1;
+			setCurrCarousel(newCurr);
+
+			if (newCurr === soulSliderLength)
+			{
+				moveToNthSlide(0);
+			}
+
+			setCarouselTransition('500ms ease-in-out');
+		}
+	};
+
+	const slidePrevSoulsCarousel = (): void =>
 	{
-		if (index)
+		if (!delayState)
 		{
-			setIndexState(index);
+			setDelayState(true);
+
+			const soulSliderLength = total;
+			const newCurr = currCarousel - 1;
+			setCurrCarousel(newCurr);
+
+			if (newCurr === 0)
+			{
+				moveToNthSlide(soulSliderLength);
+			}
+
+			setCarouselTransition('500ms ease-in-out');
 		}
-	}, [ index ]);
+	};
+
+	const moveToNthSlide = (n: number): void =>
+	{
+		setTimeout(() =>
+		{
+			setCarouselTransition('');
+			setCurrCarousel(n);
+			setDelayState(false);
+		}, 500);
+	};
 
 	return (
-		<Box data-component='Carousel' height={500} overflow='hidden' width='100%'>
-			<Stack
-				direction='row'
-				flexWrap='nowrap'
-				height='100%'
-				ref={setTrackRefState}
-				width='100%'
+		<>
+			<Box
+				data-component='Carousel'
+				height={height}
+				overflow='hidden'
+				width={width}
 			>
-				<CarouselItem>
-					{children(modulo(indexState - 1, total))}
-				</CarouselItem>
+				<Box
+					component='div'
+					height='100%'
+					position='relative'
+					width='100%'
+					sx={{
+						marginLeft: `${currCarousel * -100}%`,
+						transition: carouselTransition
+					}}
+					onTransitionEnd={() => setDelayState(false)}
+				>
+					<Box height='100%' id='prev' left='-100%' position='absolute' top={0} width='100%'>
+						{children(modulo(-1, total))}
+					</Box>
 
-				<CarouselItem data-component='test'>
-					{children(indexState)}
-				</CarouselItem>
+					{Array.from({ length: total }).map((i, j) => (
+						<Box height='100%' key={j} left={`${j * 100}%`} position='absolute' top={0} width='100%'>
+							{children(j)}
+						</Box>
+					))}
 
-				<CarouselItem>
-					{children(modulo(indexState + 1, total))}
-				</CarouselItem>
-			</Stack>
-		</Box>
+					<Box height='100%' id='next' left={`${total * 100}%`} position='absolute' top={0} width='100%'>
+						{children(modulo(0, total))}
+					</Box>
+				</Box>
+			</Box>
+
+			<button onClick={slidePrevSoulsCarousel}>left</button>
+			<button onClick={slideNextSoulsCarousel}>right</button>
+			{currCarousel}
+		</>
 	);
 }
