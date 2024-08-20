f45e36a6a5972e1fda3d0cac9602e7a6a972087e
RWB0104 <psj2716@gmail.com>
2024-08-21 01:18:29 +0900

    feat: ✨ [FEATURE] piedit 프로젝트 신규 추가

---
+++ apps/piedit/next.config.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
+})
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+// @ts-expect-error nx with PWA
+const nextConfig = withPWA({
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  swcMinify: true
+});
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
---
+++ apps/piedit/src/app/page.tsx
@@ -0,0 +1,18 @@
+/**
+ * 인덱스 페이지 컴포넌트
+ *
+ * @author RWB
+ * @since 2024.08.21 Wed 01:12:55
+ */
+
+/**
+ * 인덱스 페이지 컴포넌트 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export default function Index(): JSX.Element
+{
+	return (
+		<div />
+	);
+}
