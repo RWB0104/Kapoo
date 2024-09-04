cf91413af2206a1ba84e28f93b028da06e179e14
RWB0104 <psj2716@gmail.com>
2024-09-04 17:59:01 +0900

    feat: 🔨 [FEATURE] 에디터 상태 모듈 신규 추가

---
+++ packages/global-module/state/src/piedit/editor.ts
@@ -0,0 +1,39 @@
+/**
+ * 에디터 상태 모듈
+ *
+ * @author RWB
+ * @since 2024.09.04 Wed 17:43:10
+ */
+
+import { create } from 'zustand';
+
+export interface EditorProps
+{
+	/**
+	 * 래핑
+	 */
+	wrap: boolean;
+}
+
+export type SetEditorHandler = (editor: EditorProps) => void;
+
+export interface EditorStoreProps
+{
+	/**
+	 * 에디터
+	 */
+	editorState: EditorProps;
+
+	/**
+	 * 에디터 할당 메서드
+	 */
+	setEditorState: SetEditorHandler;
+}
+
+export const editorStore = create<EditorStoreProps>((set) => ({
+	editorState: { wrap: true },
+	setEditorState: (editor): void =>
+	{
+		set({ editorState: editor });
+	}
+}));
