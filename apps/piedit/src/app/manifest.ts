fed9245d4a51cea3b70189e5e3d3d0d16b90bf5e
RWB0104 <psj2716@gmail.com>
2024-09-08 05:35:37 +0900

    feat: 🔨 [FEATURE] 신규 스크립트 추가
    - manifest
    - 공통

---
+++ apps/piedit/src/app/manifest.ts
@@ -0,0 +1,70 @@
+/**
+ * manifest 에셋 모듈
+ *
+ * @author RWB
+ * @since 2024.09.08 Sun 05:21:57
+ */
+
+import { MetadataRoute } from 'next';
+
+/**
+ * manifest 에셋 모듈 반환 메서드
+ *
+ * @returns {MetadataRoute.Manifest} manifest
+ */
+export default function ManifestAssets(): MetadataRoute.Manifest
+{
+	return {
+		background_color: '#000000',
+		description: process.env.NEXT_PUBLIC_DESCRIPTION,
+		display: 'standalone',
+		icons: [
+			{
+				sizes: '72x72',
+				src: '/icons/icon-72x72.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '96x96',
+				src: '/icons/icon-96x96.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '128x128',
+				src: '/icons/icon-128x128.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '144x144',
+				src: '/icons/icon-144x144.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '152x152',
+				src: '/icons/icon-152x152.png',
+				type: 'image/png'
+			},
+			{
+				purpose: 'any',
+				sizes: '192x192',
+				src: '/icons/icon-192x192.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '384x384',
+				src: '/icons/icon-384x384.png',
+				type: 'image/png'
+			},
+			{
+				sizes: '512x512',
+				src: '/icons/icon-512x512.png',
+				type: 'image/png'
+			}
+		],
+		name: process.env.NEXT_PUBLIC_TITLE,
+		scope: './',
+		short_name: 'piedit',
+		start_url: './',
+		theme_color: '#FFFFFF'
+	};
+}
---
+++ apps/piedit/src/common/index.ts
@@ -0,0 +1,32 @@
+/**
+ * 공통 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.09.08 Sun 04:55:11
+ */
+
+import { BaseMetadataProps, getBaseMetadata } from '@kapoo/common';
+import { Metadata } from 'next';
+
+export type MetadataProps = Omit<BaseMetadataProps, 'sitename' | 'baseurl'>;
+
+/**
+ * 메타데이터 반환 메서드
+ *
+ * @param {MetadataProps} params: MetadataProps
+ *
+ * @returns {Metadata} 메타데이터
+ */
+export function getMetadata(params?: MetadataProps): Metadata
+{
+	const init: BaseMetadataProps = {
+		baseurl: process.env.NEXT_PUBLIC_BASE_URL,
+		description: process.env.NEXT_PUBLIC_DESCRIPTION,
+		keywords: [],
+		sitename: process.env.NEXT_PUBLIC_TITLE,
+		thumbnail: '/thumb.png',
+		url: '/'
+	};
+
+	return getBaseMetadata({ ...init, ...params });
+}
